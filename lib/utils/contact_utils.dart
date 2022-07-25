2cb8ea465fddee4620604c019cd6d6314a4e560d
dmitry2157 <80911502+dmitry2157@users.noreply.github.com>
2022-07-25 16:01:02 +0300

    Initial commit

---
+++ lib/utils/contact_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContactUtils {
+  static const List<IconData> contactIcon = [
+    Icons.home,
+    Icons.phone,
+    Icons.mail,
+  ];
+
+  static const List<String> titles = [
+    "Location",
+    "Phone",
+    "Email",
+  ];
+
+  static const List<String> details = [
+    "Saint-Petersburg, Russia",
+    "+7 921 4459698",
+    "dvpotekhin@gmail.com",
+  ];
+}
